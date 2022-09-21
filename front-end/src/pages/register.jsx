620a0b73cb4e2043efc80072d51c59d3b93bd32c
Hanna Bacelar <hannabacelar@hotmail.com>
2022-09-21 20:17:35 -0300

    feat: pagina de registro + validaçoes

---
+++ front-end/src/pages/register.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+
+function Register() {
+  // const [login, setLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [isButtonDisabled, setButtonDisabled] = useState(true);
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const verifyForm = () => {
+    const emailFormat = /[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]*\w$/;
+    const minSize = 6;
+    const minName = 12;
+    const isEmailValid = emailFormat.test(email);
+    const isPasswordValid = password.length >= minSize;
+    const isNameValid = name.length < minName;
+    setButtonDisabled(!(isEmailValid && isPasswordValid && isNameValid));
+  };
+  useEffect(() => {
+    verifyForm();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email, password, name]);
+  const handleInputChange = async (target) => {
+    if (target.name === 'name') setName(target.value);
+    if (target.name === 'email') setEmail(target.value);
+    if (target.name === 'password') setPassword(target.value);
+  };
+  return (
+    <div className="Login">
+      <form className="login-form">
+        <h1>Register</h1>
+        <label htmlFor="Nome">
+          <span>Nome</span>
+          <input
+            id="name"
+            type="text"
+            onChange={ ({ target }) => handleInputChange(target) }
+            data-testid="common_register__input-name"
+            name="nome"
+            placeholder="Nome"
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            type="text"
+            id="email"
+            data-testid="common_register__input-email"
+            name="email"
+            onChange={ ({ target }) => handleInputChange(target) }
+            placeholder="E-mail"
+          />
+        </label>
+        <label htmlFor="password">
+          <span>Password</span>
+          <input
+            id="password"
+            type="password"
+            onChange={ ({ target }) => handleInputChange(target) }
+            data-testid="common_register__input-password"
+            name="password"
+            placeholder="Senha"
+          />
+        </label>
+        <button
+          disabled={ isButtonDisabled }
+          onClick={ handleClick }
+          type="submit"
+          className="login-btn"
+          data-testid="common_login__button-login"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Register;
