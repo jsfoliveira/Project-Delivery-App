fd77b8737b10493e59331af75f51289a879ae550
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-09-22 01:49:17 -0300

    fix: :bug: Correção do erro "No 'Access-Control-Allow-Origin' header is present on the requested resource."
    Foi usado o "cors" para corrigir o erro, seguindo as dicas do vídeo e do fórum stackoverflow.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+// https://www.youtube.com/watch?v=bk661C4WY6I
+// https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+const cors = require('cors');
+
 require('express-async-errors');
 const userRoute = require('../routes/UsersRoute');
 const loginRoute = require('../routes/LoginRoute');
 const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use(userRoute);
