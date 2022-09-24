98746f3ee028cf64fe17aaec9cd4a039035ce369
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-09-24 15:18:08 -0300

    feat: :construction: Começando a tela de checkout.

--- front-end/src/pages/checkout.jsx
+++ front-end/src/pages/checkout.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <>
+      <Header />
+      <h1>Checkout</h1>
+    </>
   );
 }
 
