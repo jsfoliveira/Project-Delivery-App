8821044ac0ef8c2ea6cf1a988ff4066642596f0b
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-09-26 14:31:30 -0300

    feat: :construction: Tela de Pedidos
    Foi inicial do frontend da tela de pedidos.
    
    Co-authored-by: Juliana Oliveira <jsfoliveira89@gmail.com>

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login';
 import Customer from './pages/customer';
 import Checkout from './pages/checkout';
 import Register from './pages/register';
+import Order from './pages/order';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/customer/products" element={ <Customer /> } />
       <Route path="/customer/checkout" element={ <Checkout /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/orders" element={ <Order /> } />
     </Routes>
   );
 }
---
+++ front-end/src/components/CardOrder.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CardOrder() {
+  const [orders, setOrders] = useState([]);
+
+  // adcionar zero à esquerda do número do pedido
+  const addZeros = (num) => {
+    let numberWithZeros = String(num);
+    let counter = numberWithZeros.length;
+    const maxSize = 4;
+
+    while (counter < maxSize) {
+      numberWithZeros = `0${numberWithZeros}`;
+      counter += 1;
+    }
+
+    return numberWithZeros;
+  };
+
+  // converte a data no formato dd/mm/yy.
+  const convertDate = (data) => {
+    const now = new Date(data);
+    const result = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`;
+    return result;
+  };
+
+  // converter string do valor total
+  const convertPrice = (value) => {
+    const brlValue = value.toString().replace('.', ',');
+    return `R$ ${brlValue}`;
+  };
+
+  // datatestId
+  const dataTestidID = 'customer_orders__element-order-id-';
+  const dataTestidStatus = 'customer_orders__element-delivery-status-';
+  const dataTestidDate = 'customer_orders__element-order-date-';
+  const dataTestidPrice = 'customer_orders__element-card-price-';
+
+  // renderizando os cards com id, status, saleDate e totalPrice
+  const card = (object) => {
+    const { id, status, saleDate, totalPrice } = object;
+    return (
+      <div>
+        <Link to={ `/customer/orders/${id} ` }>
+          <p data-testid={ `${dataTestidID}-${id}` }>
+            Pedido:
+            {' '}
+            { addZeros(id) }
+          </p>
+
+          <p data-testid={ `${dataTestidStatus}-${id}` }>
+            Status:
+            {' '}
+            { status }
+          </p>
+
+          <p data-testid={ `${dataTestidDate}-${id}` }>
+            Data:
+            {' '}
+            { convertDate(saleDate) }
+          </p>
+
+          <p data-testid={ `${dataTestidPrice}-${id}` }>
+            Preço total:
+            {' '}
+            { convertPrice(totalPrice) }
+          </p>
+        </Link>
+      </div>
+    );
+  };
+
+  const renderCardOrders = () => {
+    if (orders.length !== 0 || orders !== undefined) {
+      return (
+        orders.map((element) => {
+          const object = {
+            id: element.id,
+            status: element.status,
+            saleDate: element.saleDate,
+            totalPrice: element.totalPrice,
+          };
+          return card(object);
+        })
+      );
+    }
+  };
+
+  return (
+    <div>
+      { renderCardOrders() }
+    </div>
+  );
+}
+
+export default CardOrder;
