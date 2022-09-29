e1321098a8f4548aa4cd8b54f073e4181b616070
gellwanger <gustavojce@gmail.com>
2022-09-29 17:21:32 -0300

    Add placeholders tests

--- front-end/src/tests/login.test.js
+++ front-end/src/tests/login.test.js
@@ -7,15 +7,27 @@ describe(
   () => {
     const setup = () => render(<Login />);
 
+    test('Verifica se a página possui um input com placeholder E-mail', () => {
+      setup();
+      const placeholder = screen.getByPlaceholderText('E-mail');
+      expect(placeholder).toBeInTheDocument();
+    });
+
+    test('Verifica se a página possui um input com placeholder Senha', () => {
+      setup();
+      const placeholder = screen.getByPlaceholderText('Senha');
+      expect(placeholder).toBeInTheDocument();
+    });
+
     test('Verifica se a página possui um botão com a palavra Login', () => {
       setup();
-      const button = screen.getByRole('button', { name: 'Login' }, { exact: true });
+      const button = screen.getByRole('button', { name: 'Login' });
       expect(button).toBeInTheDocument();
     });
 
     test('Verifica se a página possui um botão com a palavra Cadastra-se', () => {
       setup();
-      const button = screen.getByRole('button', { name: 'Cadastra-se' }, { exact: true });
+      const button = screen.getByRole('button', { name: 'Cadastra-se' });
       expect(button).toBeInTheDocument();
     });
   },
