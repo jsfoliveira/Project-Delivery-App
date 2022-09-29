bdd91725fbdf4ab052fff1627fb0297a5e3a45a8
gellwanger <gustavojce@gmail.com>
2022-09-29 17:14:15 -0300

    Add buttons tests

---
+++ front-end/src/tests/login.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Login from '../pages/login';
+
+describe(
+  'Testa se todos os elementos pré-definidos estão renderizados na tela de login',
+  () => {
+    const setup = () => render(<Login />);
+
+    test('Verifica se a página possui um botão com a palavra Login', () => {
+      setup();
+      const button = screen.getByRole('button', { name: 'Login' }, { exact: true });
+      expect(button).toBeInTheDocument();
+    });
+
+    test('Verifica se a página possui um botão com a palavra Cadastra-se', () => {
+      setup();
+      const button = screen.getByRole('button', { name: 'Cadastra-se' }, { exact: true });
+      expect(button).toBeInTheDocument();
+    });
+  },
+);
